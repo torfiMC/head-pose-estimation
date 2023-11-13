a83a226309792e1a1f457291da2e5e79bf39c942
Torfi Olafsson <torfi.olafsson@microsoft.com>
2023-11-13 14:46:07 -0800

    Horizontal and vertical tracking working
    use WASDQE to shift offsets
    Lookat disabled

--- textureMapper.py
+++ textureMapper.py
@@ -21,12 +21,16 @@ https://github.com/yinguobing/head-pose-estimation
 # and having it walk around on uneven terrain, as well
 # as implementing a fully rotatable camera.
 
+from pose_estimator import PoseEstimator
+from mark_detector import MarkDetector
+import cv2
+from argparse import ArgumentParser
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import CollisionTraverser, CollisionNode
 from panda3d.core import CollisionHandlerQueue, CollisionRay
 from panda3d.core import Filename, AmbientLight, DirectionalLight
-from panda3d.core import PandaNode, NodePath, Camera, TextNode
-from panda3d.core import CollideMask
+from panda3d.core import PandaNode, NodePath, Camera, TextNode, CardMaker, Texture, Vec3, OrthographicLens, Point3, TexProjectorEffect, LensNode, TextureStage
+from panda3d.core import CollideMask, PerspectiveLens
 from direct.task.Task import Task
 from direct.gui.OnscreenText import OnscreenText
 from direct.actor.Actor import Actor
@@ -36,15 +40,8 @@ import os
 import math
 
 
-from argparse import ArgumentParser
-
-import cv2
-
-from mark_detector import MarkDetector
-from pose_estimator import PoseEstimator
-
 print(__doc__)
-print("OpenCV version: {}".format(cv2.__version__))
+# print("OpenCV version: {}".format(cv2.__version__))
 
 # Parse arguments from user input.
 parser = ArgumentParser()
@@ -82,118 +79,70 @@ class RoamingRalphDemo(ShowBase):
         self.keyMap = {
             "left": 0, "right": 0, "forward": 0, "cam-left": 0, "cam-right": 0, "up": 0, "down": 0, "back": 0}
 
-        # Set up the environment
-        #
-        # This environment model contains collision meshes.  If you look
-        # in the egg file, you will see the following:
-        #
-        #    <Collide> { Polyset keep descend }
-        #
-        # This tag causes the following mesh to be converted to a collision
-        # mesh -- a mesh which is optimized for collision, not rendering.
-        # It also keeps the original mesh, so there are now two copies ---
-        # one optimized for rendering, one for collisions.
+        self.offset = [0.0, 0.0, 0.0]
+        self.lastPos = [0.0, 0.0, 0.0]
 
-        self.environ = loader.loadModel("models/world")
-        self.environ.reparentTo(render)
+        card_maker = CardMaker("quad")
+        # Define the size of the quad (full screen)
+        card_maker.setFrame(-1, 1, -1, 1)
+        self.quad = self.render.attachNewNode(card_maker.generate())
 
-        # Create the main character, Ralph
+        texture = self.loader.loadTexture("models/test2.png")
+        texture.setFormat(Texture.F_rgb)
+        self.texture = texture
+        self.quad.setTexture(texture)
+        # card_maker.setNumVertices(100)
+        card_maker.setUvRange((0, 0), (1, 1))
 
-        ralphStartPos = self.environ.find("**/start_point").getPos()
-        self.ralph = Actor("models/ralph",
-                           {"run": "models/ralph-run",
-                            "walk": "models/ralph-walk"})
-        self.ralph.reparentTo(render)
-        self.ralph.setScale(.2)
-        self.ralph.setPos(ralphStartPos + (0, 0, 0.5))
+        self.quad.setLightOff()
 
-        # Create a floater object, which floats 2 units above ralph.  We
-        # use this as a target for the camera to look at.
+        # Create a lens for the texture projection
+        proj_lens = PerspectiveLens()
+        proj_lens.setFov(40, 40)
 
-        self.floater = NodePath(PandaNode("floater"))
-        self.floater.reparentTo(self.ralph)
-        self.floater.setZ(2.0)
+        projector = self.render.attachNewNode(LensNode("projector"))
+        # projector.setPos(10, -10, 5)  # Example position
+        # projector.lookAt(self.quad)
+        print(projector.node)
+        projector.node().setLens(proj_lens)
+        self.projector = projector
+
+        ts = TextureStage('ts')
+        ts.setSort(1)
+        ts.setMode(TextureStage.MDecal)
+        self.ts = ts
+        # projector.setPos(10, -10, 5)  # Example position
+        # projector.lookAt(self.quad)
+        self.quad.projectTexture(self.ts, self.texture, self.projector)
+
+        lens = OrthographicLens()
+        lens.setFilmSize(2, 2)  # Adjust if needed, based on the quad's size
+        self.cam.node().setLens(lens)
+        self.cam.setPos(0, -10, 0)  # Camera position
+        self.cam.lookAt(self.quad)  # Look straight at the quad
+        self.gameTask = taskMgr.add(self.gameLoop, "gameLoop")
 
         # Accept the control keys for movement and rotation
 
         self.accept("escape", sys.exit)
-        # self.accept("arrow_left", self.setKey, ["left", True])
-        # self.accept("arrow_right", self.setKey, ["right", True])
-        # self.accept("arrow_up", self.setKey, ["forward", True])
-        self.accept("a", self.setKey, ["cam-left", True])
-        self.accept("d", self.setKey, ["cam-right", True])
-        self.accept("w", self.setKey, ["forward", True])
-        self.accept("s", self.setKey, ["back", True])
-        self.accept("r", self.setKey, ["up", True])
-        self.accept("f", self.setKey, ["down", True])
-        # self.accept("arrow_left-up", self.setKey, ["left", False])
-        # self.accept("arrow_right-up", self.setKey, ["right", False])
-        # self.accept("arrow_up-up", self.setKey, ["forward", False])
-        self.accept("a-up", self.setKey, ["cam-left", False])
-        self.accept("d-up", self.setKey, ["cam-right", False])
-        self.accept("w-up", self.setKey, ["forward", False])
-        self.accept("s-up", self.setKey, ["back", False])
-        self.accept("r-up", self.setKey, ["up", False])
-        self.accept("f-up", self.setKey, ["down", False])
-        taskMgr.add(self.move, "moveTask")
+        self.accept("space", self.reset)
 
-        # Game state variables
-        self.isMoving = False
+        self.accept("a", self.setKey, ["left", True])
+        self.accept("d", self.setKey, ["right", True])
+        self.accept("w", self.setKey, ["up", True])
+        self.accept("s", self.setKey, ["down", True])
+        self.accept("q", self.setKey, ["forward", True])
+        self.accept("e", self.setKey, ["back", True])
+
+        self.accept("a-up", self.setKey, ["left", False])
+        self.accept("d-up", self.setKey, ["right", False])
+        self.accept("w-up", self.setKey, ["up", False])
+        self.accept("s-up", self.setKey, ["down", False])
+        self.accept("q-up", self.setKey, ["forward", False])
+        self.accept("e-up", self.setKey, ["back", False])
 
         # Set up the camera
         self.disableMouse()
-        self.camera.setPos(self.ralph.getX(), self.ralph.getY() + 10, 2)
-
-        # We will detect the height of the terrain by creating a collision
-        # ray and casting it downward toward the terrain.  One ray will
-        # start above ralph's head, and the other will start above the camera.
-        # A ray may hit the terrain, or it may hit a rock or a tree.  If it
-        # hits the terrain, we can detect the height.  If it hits anything
-        # else, we rule that the move is illegal.
-        self.cTrav = CollisionTraverser()
-
-        self.ralphGroundRay = CollisionRay()
-        self.ralphGroundRay.setOrigin(0, 0, 9)
-        self.ralphGroundRay.setDirection(0, 0, -1)
-        self.ralphGroundCol = CollisionNode('ralphRay')
-        self.ralphGroundCol.addSolid(self.ralphGroundRay)
-        self.ralphGroundCol.setFromCollideMask(CollideMask.bit(0))
-        self.ralphGroundCol.setIntoCollideMask(CollideMask.allOff())
-        self.ralphGroundColNp = self.ralph.attachNewNode(self.ralphGroundCol)
-        self.ralphGroundHandler = CollisionHandlerQueue()
-        self.cTrav.addCollider(self.ralphGroundColNp, self.ralphGroundHandler)
-
-        self.camGroundRay = CollisionRay()
-        self.camGroundRay.setOrigin(0, 0, 9)
-        self.camGroundRay.setDirection(0, 0, -1)
-        self.camGroundCol = CollisionNode('camRay')
-        self.camGroundCol.addSolid(self.camGroundRay)
-        self.camGroundCol.setFromCollideMask(CollideMask.bit(0))
-        self.camGroundCol.setIntoCollideMask(CollideMask.allOff())
-        self.camGroundColNp = self.camera.attachNewNode(self.camGroundCol)
-        self.camGroundHandler = CollisionHandlerQueue()
-        self.cTrav.addCollider(self.camGroundColNp, self.camGroundHandler)
-
-        # Uncomment this line to see the collision rays
-        # self.ralphGroundColNp.show()
-        # self.camGroundColNp.show()
-
-        # Uncomment this line to show a visual representation of the
-        # collisions occuring
-        # self.cTrav.showCollisions(render)
-
-        # Create some lighting
-        ambientLight = AmbientLight("ambientLight")
-        ambientLight.setColor((.3, .3, .3, 1))
-        directionalLight = DirectionalLight("directionalLight")
-        directionalLight.setDirection((-5, -5, -5))
-        directionalLight.setColor((1, 1, 1, 1))
-        directionalLight.setSpecularColor((1, 1, 1, 1))
-        render.setLight(render.attachNewNode(ambientLight))
-        render.setLight(render.attachNewNode(directionalLight))
-
-        self.camLens.setFov(120)
-        # Before estimation started, there are some startup works to do.
 
         # 1. Setup the video source from webcam or video file.
         self.video_src = args.cam if args.cam is not None else args.video
@@ -216,12 +165,14 @@ class RoamingRalphDemo(ShowBase):
         # 4. Measure the performance with a tick meter.
         self.tm = cv2.TickMeter()
 
-        self.gameTask = taskMgr.add(self.gameLoop, "gameLoop")
-
         self.translation_vector = [[0, 0, 0]]
         self.x = 0
         self.y = 0
         self.z = 0
+
+        self.xoffset = 0.0
+        self.yoffset = 0.0
+        self.zoffset = 45.0
 
     # Records the state of the arrow keys
 
@@ -238,7 +189,30 @@ class RoamingRalphDemo(ShowBase):
         #    return Task.cont
         self.captureFrame()
 
-        return Task.cont    # Since every return is Task.cont, the task will
+        if self.keyMap["left"]:
+            self.xoffset -= 1.0
+            print("xoffset ", self.xoffset)
+        if self.keyMap["right"]:
+            self.xoffset += 1.0
+            print("xoffset ", self.xoffset)
+        if self.keyMap["up"]:
+            self.zoffset += 1.0
+            print("zoffset ", self.zoffset)
+        if self.keyMap["down"]:
+            self.zoffset -= 1.0
+            print("zoffset ", self.zoffset)
+
+        if self.keyMap["forward"]:
+            self.yoffset += 10.0
+            print("yoffset ", self.yoffset)
+        if self.keyMap["back"]:
+            self.yoffset -= 10.0
+            print("yoffset ", self.yoffset)
+
+        #    self.ralph.setY(self.ralph, -25 * dt)
+
+        return Task.cont
+     # Since every return is Task.cont, the task will
         # continue indefinitely
 
     def captureFrame(self):
@@ -248,8 +222,8 @@ class RoamingRalphDemo(ShowBase):
             return
 
         # If the frame comes from webcam, flip it so it looks like a mirror.
-        if self.video_src == 0:
-            frame = cv2.flip(frame, 2)
+        # if self.video_src == 0:
+        #    frame = cv2.flip(frame, 2)
 
         # Step 1: Get a face from current frame.
         facebox = self.mark_detector.extract_cnn_facebox(frame)
@@ -263,9 +237,9 @@ class RoamingRalphDemo(ShowBase):
             face_img = frame[y1: y2, x1: x2]
 
             # Run the detection.
-            self.tm.start()
+            # self.tm.start()
             marks = self.mark_detector.detect_marks(face_img)
-            self.tm.stop()
+            # self.tm.stop()
 
             # Convert the locations from local face area to the global image.
             marks *= (x2 - x1)
@@ -286,19 +260,29 @@ class RoamingRalphDemo(ShowBase):
             self.translation_vector = pose[1]
             # print(self.translation_vector[0])
             scale = 0.1
-            # self.camera.setX(self.translation_vector[0] * scale)
-            # self.camera.setY(self.translation_vector[2] * scale)
-            # self.camera.setZ(self.translation_vector[1] * scale + 2)
 
-            self.camera.setPosHpr(self.translation_vector[0] * scale * -1 + self.x,
-                                  self.translation_vector[2] * scale + self.y,
-                                  self.translation_vector[1] *
-                                  scale + 20 + self.z,
-                                  0.0,
-                                  0.0,
-                                  0.0)
+            # Calculate the magnitude of the vector
+            magnitude = math.sqrt(
+                self.translation_vector[0]**2 + self.translation_vector[1]**2 + self.translation_vector[2]**2)
 
-            # self.camera.lookAt(self.floater)
+            # print(self.translation_vector)
+
+            magnitude = 1500.0
+
+            x = (self.xoffset + self.translation_vector[0]) / magnitude
+            # exchange z and y since it's left handed vs right handed coordinates system
+            y = (self.yoffset + self.translation_vector[2]) / magnitude
+            z = (self.zoffset + self.translation_vector[1]) / magnitude
+            # print(x, y, z)
+
+            self.x = x
+            self.y = y
+            self.z = z
+
+            self.projector.setPos(x, y, z)  # Example position
+            # self.projector.lookAt(self.quad)
+            # print(self.translation_vector)
+            # self.quad.projectTexture(self.ts, self.texture, self.projector)
 
             # Do you want to see the head axes?
             # self.pose_estimator.draw_axes(frame, pose[0], pose[1])
@@ -312,109 +296,12 @@ class RoamingRalphDemo(ShowBase):
         # Show preview.
         # cv2.imshow("Preview", frame)
 
+    def reset(self):
+        self.xoffset = -self.x
+        self.yoffset = -self.y
+
     def setKey(self, key, value):
         self.keyMap[key] = value
-
-    # Accepts arrow keys to move either the player or the menu cursor,
-    # Also deals with grid checking and collision detection
-    def move(self, task):
-
-        # Get the time that elapsed since last frame.  We multiply this with
-        # the desired speed in order to find out with which distance to move
-        # in order to achieve that desired speed.
-        dt = globalClock.getDt()
-
-        # If the camera-left key is pressed, move camera left.
-        # If the camera-right key is pressed, move camera right.
-
-        step = 100
-
-        if self.keyMap["cam-left"]:
-            self.x -= step * dt
-            print((self.x, self.y, self.z))
-        if self.keyMap["cam-right"]:
-            self.x += step * dt
-            print((self.x, self.y, self.z))
-
-        if self.keyMap["forward"]:
-            self.y += step * dt
-            print((self.x, self.y, self.z))
-
-        if self.keyMap["back"]:
-            self.y -= step * dt
-            print((self.x, self.y, self.z))
-
-        if self.keyMap["down"]:
-            self.z -= step * dt
-            print((self.x, self.y, self.z))
-        if self.keyMap["up"]:
-            self.z += step * dt
-            print((self.x, self.y, self.z))
-
-        #    self.camera.setX(self.camera, +20 * dt)
-
-        # save ralph's initial position so that we can restore it,
-        # in case he falls off the map or runs into something.
-
-        startpos = self.ralph.getPos()
-
-        # If a move-key is pressed, move ralph in the specified direction.
-
-        # if self.keyMap["left"]:
-        #    self.ralph.setH(self.ralph.getH() + 300 * dt)
-        # if self.keyMap["right"]:
-        #    self.ralph.setH(self.ralph.getH() - 300 * dt)
-        # if self.keyMap["forward"]:
-        #    self.ralph.setY(self.ralph, -25 * dt)
-
-        # If ralph is moving, loop the run animation.
-        # If he is standing still, stop the animation.
-
-        # if self.keyMap["forward"] or self.keyMap["left"] or self.keyMap["right"]:
-        #    if self.isMoving is False:
-        # self.ralph.loop("run")
-        #        self.isMoving = True
-        # else:
-        #    if self.isMoving:
-        #        self.ralph.stop()
-        #        self.ralph.pose("walk", 5)
-        #        self.isMoving = False
-
-        # If the camera is too far from ralph, move it closer.
-        # If the camera is too close to ralph, move it farther.
-
-        camvec = self.ralph.getPos() - self.camera.getPos()
-        # camvec.setZ(0)
-        # camdist = camvec.length()
-        # camvec.normalize()
-        # if camdist > 10.0:
-        #    self.camera.setPos(self.camera.getPos() + camvec * (camdist - 10))
-        #    camdist = 10.0
-        # if camdist < 5.0:
-        #    self.camera.setPos(self.camera.getPos() - camvec * (5 - camdist))
-        #    camdist = 5.0
-
-        # Normally, we would have to call traverse() to check for collisions.
-        # However, the class ShowBase that we inherit from has a task to do
-        # this for us, if we assign a CollisionTraverser to self.cTrav.
-        # self.cTrav.traverse(render)
-
-        # Adjust ralph's Z coordinate.  If ralph's ray hit terrain,
-        # update his Z. If it hit anything else, or didn't hit anything, put
-        # him back where he was last frame.
-
-        entries = list(self.ralphGroundHandler.entries)
-        entries.sort(key=lambda x: x.getSurfacePoint(render).getZ())
-
-        if len(entries) > 0 and entries[0].getIntoNode().name == "terrain":
-            self.ralph.setZ(entries[0].getSurfacePoint(render).getZ())
-        else:
-            self.ralph.setPos(startpos)
-
-        # Keep the camera at one foot above the terrain,
-        # or two feet above ralph, whichever is greater.
-
-        return task.cont
 
 
 demo = RoamingRalphDemo()
